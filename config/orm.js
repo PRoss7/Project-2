5fb8082cf04109d209292f2fb0e26dc114f3cbd0
debradaise <dadaise@gmail.com>
2017-12-09 13:16:09 -0500

    orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 var connection = require("../config/connection.js");
 
-connection.connect(function (err) {
+connection.connect(function(err) {
 
     if (err) {
         console.log("error connection: " + err.stack);
@@ -13,57 +13,80 @@ connection.connect(function (err) {
 
 var orm = {
 
-    addMedia: function (media, cb) {
+    addMedia: function(media, cb) {
 
-        var queryString = "INSERT INTO wishlist VALUES ??";
-        connection.query(queryString, function (err, result) {
+        var queryString = "INSERT INTO wishlist VALUES ???";
+        connection.query(queryString, [media.title, media.rating, media.genre], function(err, result) {
 
             if (err) {
                 throw err;
             }
 
-            cb(resu
+            cb(result)
 
         });
 
     },
 
-    moveMedia: function (id, cb) {
-
-        connection.query("DELETE FROM wishlist", function (err, result) {
-
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-
-        });
-
-        connection.query("INSERT INTO viewed (Title, Rating, Genre) VALUES (?, ?, ?)", [Title, Rating, Genre], function (err, result) {
+    moveMedia: function(id, cb) {
+        connection.query("SELECT * FROM wishlist WHERE id = ?", [id], function(err, result) {
 
             if (err) {
                 throw err;
             }
+            var mediaToMove = result;
 
-            cb(result);
+            connection.query("INSERT INTO viewed (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.title, media.ToMove.rating, mediaToMove.genre], function(err, result) {
+
+                if (err) {
+                    throw err;
+                }
+
+                connection.query("DELETE FROM wishlist WHERE id=?", [id], function(err, result) {
+
+                    if (err) {
+                        throw err;
+                    }
+
+                    cb(result);
+
+                });
+
+
+            });
 
         });
-
     },
 
-    deleteMedia: function (id, cb) {
+    deleteMedia: function(id, cb) {
 
-        connection.query("SELECT * FROM wishlist", function (err, result) {
+        connection.query("SELECT * FROM wishlist WHERE id = ?", [id], function(err, result) {
 
             if (err) {
                 throw err;
             }
+            var mediaToMove = result;
 
-            cb(result);
+            connection.query("INSERT INTO deleted (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.title, media.ToMove.rating, mediaToMove.genre], function(err, result) {
+
+                if (err) {
+                    throw err;
+                }
+
+                connection.query("DELETE FROM wishlist WHERE id=?", [id], function(err, result) {
+
+                    if (err) {
+                        throw err;
+                    }
+
+                    cb(result);
+
+                });
+
+
+            });
 
         });
-
-    }
+    },
 
 };
