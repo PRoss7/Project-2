072a889b741f4e3573f619adcb2fbc132d9473b4
debradaise <dadaise@gmail.com>
2017-12-09 13:54:50 -0500

    models app.js

---
+++ models/app.js
@@ -0,0 +1,26 @@
+// Import the ORM to create functions that will interact with the database.
+
+var orm = require("../config/orm.js");
+
+var media = {
+
+    addMedia: function(media, cb) {
+        orm.addMedia(media, function(res) {
+            cb(res);
+        });
+    },
+
+    moveMedia: function(id, cb) {
+        orm.moveMedia(id, function(res) {
+            cb(res);
+        });
+    },
+
+    deleteMedia: function(id, cb) {
+        orm.deleteMedia(id, function(res) {
+            cb(res);
+        });
+    },
+}
+
+module.exports = media;
