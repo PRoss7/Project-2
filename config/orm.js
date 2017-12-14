66af78746bca3b8f8621ac3deefcd4bc9ecfb6b9
debradaise <dadaise@gmail.com>
2017-12-14 18:15:30 -0500

    working on orm

--- config/orm.js
+++ config/orm.js
@@ -1,54 +1,80 @@
 var connection = require("../config/connection.js");
 
-connection.connect(function(err) {
+var ob = {
+    Title: 'testing',
+    Rating: 'testing',
+    Genre: 'testing',
+    Media: 'testing'
+};
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
 
-    if (err) {
-        console.log("error connection: " + err.stack);
-        return;
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+            console.log(arr);
+        }
     }
 
-    console.log("connected as id: " + connection.threadId);
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
 
-});
-
+}
+//objToSql(ob);
+//console.log(objToSql(ob));
+var vals = ['2wish', '2list', '2add', '2on'];
+// Object for all our SQL statement functions.
 var orm = {
 
-    addMedia: function(media, cb) {
+    addMediaToWishlist: function(vals, cb) { //adds line into wishlist table 
 
-        var queryString = "INSERT INTO wishlist VALUES ???";
-        connection.query(queryString, [media.title, media.rating, media.genre], function(err, result) {
-
+        var queryString = "INSERT INTO wishlist (Title, Rating, Genre, Media) VALUES (?, ?, ?, ?)";
+        //connection.query(queryString, [media.title, media.rating, media.genre, media.media], function(err, result) {
+        connection.query(queryString, vals, function(err, result) {
             if (err) {
                 throw err;
             }
 
-            cb(result)
+            console.log(queryString);
 
         });
 
     },
 
-    moveMedia: function(id, cb) {
+
+
+    moveMediaToViewed: function(id, cb) { //deletes media from "wishlist" table and adds it to "viewed" table
         connection.query("SELECT * FROM wishlist WHERE id = ?", [id], function(err, result) {
 
             if (err) {
                 throw err;
             }
+            console.log("move media" + result);
             var mediaToMove = result;
 
-            connection.query("INSERT INTO viewed (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.title, media.ToMove.rating, mediaToMove.genre], function(err, result) {
+            connection.query("INSERT INTO viewed (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.Title, mediaToMove.Rating, mediaToMove.Genre], function(err, result) {
 
                 if (err) {
                     throw err;
                 }
 
-                connection.query("DELETE FROM wishlist WHERE id=?", [id], function(err, result) {
+                connection.query("DELETE FROM wishlist WHERE id = ?", [id], function(err, result) {
 
                     if (err) {
                         throw err;
                     }
 
-                    cb(result);
+
 
                 });
 
@@ -58,35 +84,94 @@ var orm = {
         });
     },
 
-    deleteMedia: function(id, cb) {
+    deleteMediaFromWishlist: function(id) { //deletes media from wishlist table and adds it to the "deleted" table
 
         connection.query("SELECT * FROM wishlist WHERE id = ?", [id], function(err, result) {
 
             if (err) {
                 throw err;
             }
-            var mediaToMove = result;
+            //  var mediaToMove = objToSql(result);
 
-            connection.query("INSERT INTO deleted (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.title, media.ToMove.rating, mediaToMove.genre], function(err, result) {
+            console.log(JSON.stringify(result, null, 2));
+
+            var mediaToMove = result[0];
+            // var queryString = "INSERT INTO deleted"
+            // queryString += mediaToMove;
+
+
+            // console.log("delete from wishlist " + mediaToMove.RowDataPacket);
+            // console.log("try" + queryString);
+            connection.query("INSERT INTO deleted (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.Title, mediaToMove.Rating, mediaToMove.Genre], function(err, result) {
+                // connection.query("INSERT INTO deleted (Title, Rating, Genre) VALUES (?, ?, ?)", [mediaToMove.Title, mediaToMove.Rating, mediaToMove.Genre], function(err, result) {
 
                 if (err) {
                     throw err;
                 }
 
-                connection.query("DELETE FROM wishlist WHERE id=?", [id], function(err, result) {
+                //     connection.query("DELETE FROM wishlist WHERE id=?", [id], function(err, result) {
 
-                    if (err) {
-                        throw err;
-                    }
+                //         if (err) {
+                //             throw err;
+                //         }
 
-                    cb(result);
 
-                });
 
+                //     });
 
             });
 
         });
     },
+
 
 };
+var all = function(id) {
+    var queryString = "SELECT * FROM wishlist WHERE id = ?";
+    connection.query(queryString, [id], function(err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+    });
+};
+
+//all(13);
+// all: function (tableInput, cb) {
+//     var queryString = "SELECT * FROM " + tableInput + ";";
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(result);
+//     });
+//   },
+
+// An example of objColVals would be {name: panther, sleepy: true}
+// update: function(table, objColVals, condition, cb) {
+//         var queryString = "UPDATE " + table;
+
+//         queryString += " SET ";
+//         queryString += objToSql(objColVals);
+//         queryString += " WHERE ";
+//         queryString += condition;
+
+//         console.log(queryString);
+//         connection.query(queryString, function(err, result) {
+//             if (err) {
+//                 throw err;
+//             }
+
+//             cb(result);
+//         });
+//     },
+//testing
+
+//orm.addMediaToWishlist(vals);
+
+//orm.moveMediaToViewed(11);
+
+orm.deleteMediaFromWishlist(3);
+
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -12,10 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(methodOverride("_method"));
 
-var routes = require("./controller.app_controller.js");
+var routes = require("./controller/app_controller.js");
 
 app.use("/", routes);
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
-}); 
+
+    //testing area
+
+    router.post();
+
+
+
+
+
+});
