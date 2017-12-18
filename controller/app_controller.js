5d859f277ba391fe21f11be936d9b5d3229c8cea
debradaise <dadaise@gmail.com>
2017-12-18 18:26:02 -0500

    touches on app.js

--- controller/app_controller.js
+++ controller/app_controller.js
@@ -1,18 +1,18 @@
 var models = require("../models/app.js");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
 
-    app.post("/media/add", function (req, res) {
+    app.post("/media/add", function(req, res) {
         var body = req.body
-        /**
-         * {
-         *   "Title": "Pulp Fiction",
-         *   "Rating": "4",
-         *   "Genre": "Action/Thriller",
-         *   "Media": "Movie"
-         * }
-         */
+            /**
+             * {
+             *   "Title": "Pulp Fiction",
+             *   "Rating": "4",
+             *   "Genre": "Action/Thriller",
+             *   "Media": "Movie"
+             * }
+             */
 
         //console.log(JSON.stringify(media, null, 2))
 
@@ -20,7 +20,7 @@ module.exports = function (app) {
             body.Title, body.Rating, body.Genre, body.Media
         ]
 
-        models.addMedia(mediaToReplacePlaceholders, function (result) {
+        models.addMedia(mediaToReplacePlaceholders, function(result) {
 
             res.json(result)
                 //res.redirect
@@ -30,25 +30,20 @@ module.exports = function (app) {
 
     })
 
-    app.post("/media/:id/move", function (req, res) {
+    app.post("/media/:id/move", function(req, res) {
         console.log("id to move " + req.params.id)
 
-        models.moveMedia(req.params.id, function () {
+        models.moveMedia(req.params.id, function() {
 
             res.status(204).json({})
 
         })
     })
 
-<<<<<<< HEAD
     app.post("/media/:id/delete", function(req, res) {
         console.log("id to delete " + req.params.id)
-=======
-    app.post("/media/:id/move", function (req, res) {
-        console.log("id to move " + req.params.id)
->>>>>>> 25a3b44b134bca567c8b728d9a301aab1cac36b9
 
-        models.deleteMedia(req.params.id, function () {
+        models.deleteMedia(req.params.id, function() {
 
             res.status(204).json({})
 
