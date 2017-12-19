486004c21a23d02825e459bffa4bc32c79dd73b4
Preston Ross <prestonross77@gmail.com>
2017-12-19 18:07:42 -0500

    files

--- public/startbootstrap-grayscale-gh-pages/js/app.js
+++ public/startbootstrap-grayscale-gh-pages/js/app.js
@@ -1,49 +1,49 @@
 
-function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function(e, r) { return t[n][r] }) } return res }
+function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
 
 
 
 
-$(function() {
+$(function () {
 
 
-  var searchCategory;
-  var mediaId = 1;
+    var searchCategory;
+    var mediaId = 1;
 
-    $("form").on("submit", function(e) {
-      console.log("success");
-       e.preventDefault();       
-       $("#results").empty();
-       // prepare the request
-       var request = gapi.client.youtube.search.list({
+    $("form").on("submit", function (e) {
+        console.log("success");
+        e.preventDefault();
+        $("#results").empty();
+        // prepare the request
+        var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
-            q: encodeURIComponent($("#mediaInput").val() + " trailer").replace(/%20/g, "+" ),
-            maxResults: 1,                       
-       }); 
-       // execute the request
-       request.execute(function(response) {        
-        //console.log(response);
-          var results = response.result;
-          //$("#results").html("");
-          $.each(results.items, function(index, item) {
-            $.get("startbootstrap-grayscale-gh-pages/tpl/item.html", function(data){
-              $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
-            });           
-          });
-          //   $.get("tpl/item.html", function(data) {
-          //       $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
-          //  $("#results").append(item.id.videoId+" "+item.snippet.title+"<br>");
-          //   });
-          // });
-          resetVideoHeight();
-       });
+            q: encodeURIComponent($("#mediaInput").val() + " trailer").replace(/%20/g, "+"),
+            maxResults: 1,
+        });
+        // execute the request
+        request.execute(function (response) {
+            //console.log(response);
+            var results = response.result;
+            //$("#results").html("");
+            $.each(results.items, function (index, item) {
+                $.get("startbootstrap-grayscale-gh-pages/tpl/item.html", function (data) {
+                    $("#results").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
+                });
+            });
+            //   $.get("tpl/item.html", function(data) {
+            //       $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            //  $("#results").append(item.id.videoId+" "+item.snippet.title+"<br>");
+            //   });
+            // });
+            resetVideoHeight();
+        });
 
     });
-    
+
     $(window).on("resize", resetVideoHeight);
 
-    $("#searchDropdown li").on("click", function() {
+    $("#searchDropdown li").on("click", function () {
 
 
         $("#search").removeAttr("disabled");
@@ -51,46 +51,46 @@ $(function() {
         searchCategory = $(this).attr("data-category");
 
 
-      //alert(searchCategory);
+        //alert(searchCategory);
 
     });
 
-    $(".dropdown a").on("click", function() {
+    $(".dropdown a").on("click", function () {
 
-      $("button", $(this).parent().parent().parent()).html($(this).html());
+        $("button", $(this).parent().parent().parent()).html($(this).html());
 
     });
 
-     $("#addToList").on("click", function (event, data) {
+    $("#addToList").on("click", function (event, data) {
 
-        
+
         $.ajax('/search', {
-          type: "POST",
-          dataType: 'json',
-          data: {
-            'search': $('#mediaInput').val().trim()
-          }
-        }).done(function(data) {
+            type: "POST",
+            dataType: 'json',
+            data: {
+                'search': $('#mediaInput').val().trim()
+            }
+        }).done(function (data) {
             console.log("movie data: ", data); //Movie data is here
 
             var button1 = $('<button type = "button" id = "addToViewed" class= "btn btn-primary"> <i class="fa fa-check" aria-hidden="true"> </i> </button>');
             var button2 = $('<button type="button" id="addToDelete" class="btn btn-danger"> <i class="fa fa-trash" aria-hidden="true"></i> </button>');
             //viewed button on the tables
-            button1.on('click', function() {
-              //alert(data.id + ' was clicked');
-              $(this).parent().parent().appendTo($('.viewedTable tbody'))
+            button1.on('click', function () {
+                //alert(data.id + ' was clicked');
+                $(this).parent().parent().appendTo($('.viewedTable tbody'))
             });
             //
-            button2.on('click', function() {
-              //alert(data.id + ' was clicked');
-              $(this).parent().parent().appendTo($('.deletedTable tbody'))
+            button2.on('click', function () {
+                //alert(data.id + ' was clicked');
+                $(this).parent().parent().appendTo($('.deletedTable tbody'))
             });
 
             var row = $("<tr></tr>");
 
             row.append($("<td>" + mediaId + "</td>"));
             mediaId++;
-            row.append($("<td>" + data.title + "</td>"));            
+            row.append($("<td>" + data.title + "</td>"));
             row.append($("<td>" + data.vote_average + "/10" + "</td>"));
             row.append($("<td>" + data.genre_ids[0] + "</td>")); // Need another API call to get genre name
             row.append($("<td>" + searchCategory + "</td>"));
@@ -103,35 +103,39 @@ $(function() {
 
             // Reload the page to get the updated list
             //location.reload();
-          }
-        );
+        }
+            );
 
-     });
-     
+    });
+
 });
 
 function resetVideoHeight() {
-    $(".video").css("height", $("#results").width() * 9/16);
+
+    $(".video").css("height", $("#results").width() * 9 / 16);
+
 }
 
 function init() {
+
     gapi.client.setApiKey("AIzaSyA388q_XO6_LZrxMTfw41reoBplb--6tds");
-    gapi.client.load("youtube", "v3", function() {
+    gapi.client.load("youtube", "v3", function () {
         // yt api is ready
     });
 
 };
 
-$(document).ready(function() {
-    $("#addToList").click(function() { //adds to wishlist table
+$(document).ready(function () {
+
+    $("#addToList").click(function (event, data) { //adds to wishlist table
         event.preventDefault();
         console.log("wishlist button fired");
         //mediaInput is placeholder for OMBD object
         var newMedia = {
-            Title: $("#mediaInput").val().trim(),
-            Rating: $("#mediaInput").val().trim(),
-            Genre: $("#mediaInput").val().trim(),
-            Media: $("#mediaInput").val().trim()
+            Title: $(data.title).val().trim(),
+            Rating: $(data.vote_average).val().trim(),
+            Genre: $(data.genre_ids[0]).val().trim(),
+            Media: $(searchCategory).val().trim()
         };
 
         console.log(newMedia);
@@ -139,26 +143,28 @@ $(document).ready(function() {
             type: "POST",
             data: newMedia
         }).then(
-            function() {
+            function () {
                 console.log("added to wishlist table");
                 // Reload the page to get the updated list
                 location.reload();
             }
-        );
+            );
+
     });
+
 });
 
 //below this line may not work
 
-$("#addToViewed").on("submit", function(event) {
+$("#addToViewed").on("submit", function (event, data) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newMedia = {
-        Title: $("#").val().trim(),
-        Rating: $("#").val().trim(),
-        Genre: $("#").val().trim(),
-        Media: $("#").val().trim()
+        Title: $(data.title).val().trim(),
+        Rating: $(data.vote_average).val().trim(),
+        Genre: $(data.genre_ids[0]).val().trim(),
+        Media: $(searchCategory).val().trim()
     };
 
     // Send the POST request.
@@ -166,23 +172,24 @@ $("#addToViewed").on("submit", function(event) {
         type: "POST",
         data: newMedia
     }).then(
-        function() {
+        function () {
             console.log("added to viewed table");
             // Reload the page to get the updated list
             location.reload();
         }
-    );
+        );
+
 });
 
-$("#deleted-section").on("submit", function(event) {
+$("#deleted-section").on("submit", function (event, data) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newMedia = {
-        Title: $("#").val().trim(),
-        Rating: $("#").val().trim(),
-        Genre: $("#").val().trim(),
-        Media: $("#").val().trim()
+        Title: $(data.title).val().trim(),
+        Rating: $(data.vote_average).val().trim(),
+        Genre: $(data.genre_ids[0]).val().trim(),
+        Media: $(searchCategory).val().trim()
     };
 
     // Send the POST request.
@@ -190,27 +197,12 @@ $("#deleted-section").on("submit", function(event) {
         type: "POST",
         data: newMedia
     }).then(
-        function() {
+        function () {
             console.log("deleted from wishlist table");
             // Reload the page to get the updated list
             location.reload();
         }
-    );
+        );
 
-<<<<<<< HEAD
 });
-=======
-    });
-});
-=======
-}
-/*
-for (var i = 0; i < $("#wishListTable").length; i++) {
-        var id = 1;
 
-
-
-       }
-*/
-
->>>>>>> 0c3e2b0624f4033c72c9014c6c32923d0e010963
